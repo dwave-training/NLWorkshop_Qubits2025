df679fa753ed99d5d2dd523eca10e81bdd01cd2b
Sara Jamous <129776424+sarajamous@users.noreply.github.com>
2025-02-24 16:39:28 -0800

    Update code_from_presentation.py

--- code_from_presentation.py
+++ code_from_presentation.py
@@ -3,12 +3,12 @@ import numpy as np
 from dwave.system import LeapHybridNLSampler
 model = Model()
 matrix = np.array([
-[0, 3, 9, 5, 1],
-[3, 0, 2, 6, 4],
-[9, 2, 0, 7, 8],
-[5, 6, 7, 0, 2],
-[1, 4, 8, 2, 0],
-])
+		[0, 3, 9, 5, 1],
+		[3, 0, 2, 6, 4],
+		[9, 2, 0, 7, 8],
+		[5, 6, 7, 0, 2],
+		[1, 4, 8, 2, 0],
+		])
 D = model.constant(matrix)
 x = model.list(5)
 model.minimize((D[x[:-1], x[1:]]).sum()+ (D[x[-1], x[0]]))
